5ecda9d27a0dcc07d30705c7c4328cf587299f15
sayed <sayed.ahmed@nextory.com>
2023-07-16 18:33:21 +0530

    initial commit

---
+++ src/test/java/com/skaffolddemo/local/LocalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skaffolddemo.local;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
