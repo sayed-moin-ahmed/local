9997b6eb9d4a64035b26ffdeafeac0de5b4861cd
sayed <sayed.ahmed@nextory.com>
2023-07-17 14:02:48 +0530

    Test Controller

---
+++ src/main/java/com/skaffolddemo/local/controller/Test.java
@@ -0,0 +1,13 @@
+package com.skaffolddemo.local.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Test {
+
+    @RequestMapping("/")
+    public String test(){
+        return "Hello!";
+    }
+}
