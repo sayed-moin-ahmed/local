c00424d63340176f751b4b57e39731e1cdc1b80c
sayed <sayed.ahmed@nextory.com>
2023-07-24 07:36:45 +0530

    Current Thread...

--- src/main/java/com/skaffolddemo/local/controller/Test.java
+++ src/main/java/com/skaffolddemo/local/controller/Test.java
@@ -1,13 +1,18 @@
 package com.skaffolddemo.local.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class Test {
+    @Autowired
 
     @RequestMapping("/hello")
     public String test(){
+        log.info("Thread Name:{}", Thread.currentThread().getName());
         return "Hello!";
     }
 }
