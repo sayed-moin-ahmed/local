c79dcd49ad3a9c5ade07ac11aabb1839bcd6d790
sayed <sayed.ahmed@nextory.com>
2023-08-03 21:50:50 +0530

    Server setup

--- src/main/java/com/skaffolddemo/local/LocalApplication.java
+++ src/main/java/com/skaffolddemo/local/LocalApplication.java
@@ -1,10 +1,8 @@
 package com.skaffolddemo.local;
 
-import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 public class LocalApplication {
